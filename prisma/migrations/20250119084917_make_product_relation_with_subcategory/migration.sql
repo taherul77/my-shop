72420f6a62b74565a1bc90cf911570034aefc00f
SK Taherul Islam Taz <taherul15-2317@diu.edu.bd>
2025-01-19 14:57:23 +0600

    add product route

---
+++ prisma/migrations/20250119084917_make_product_relation_with_subcategory/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `product` ADD COLUMN `subCategoryId` INTEGER NULL;
+
+-- AddForeignKey
+ALTER TABLE `Product` ADD CONSTRAINT `Product_subCategoryId_fkey` FOREIGN KEY (`subCategoryId`) REFERENCES `SubCategory`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
---
+++ src/app/api/product/route.tsx
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '../../../../prisma/client';
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  console.log(body);
+
+  try {
+    const newProduct = await prisma.product.create({
+      data: {
+        name: body.name,
+        description: body.description,
+        price: body.price,
+        categoryId: body.categoryId,
+        subCategoryId: body.subCategoryId || null,
+      },
+    });
+    return NextResponse.json(newProduct, { status: 201 });
+  } catch (error) {
+    console.error('Error creating product:', error);
+    return NextResponse.json({ error: error instanceof Error ? error.message : 'Error creating product' }, { status: 500 });
+  }
+}
