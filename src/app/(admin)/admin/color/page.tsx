f0450238cf40970ec958cbe1da569a1aaeec63f2
SK Taherul Islam Taz <taherul15-2317@diu.edu.bd>
2025-01-22 18:40:57 +0600

    update project

--- src/app/(admin)/admin/color/page.tsx
+++ src/app/(admin)/admin/color/page.tsx
@@ -1,7 +1,11 @@
 import ColorComponent from '@/components/Admin/Color/ColorComponent'
 import React from 'react'
+import { prisma } from '../../../../../prisma/client';
 
-const ColorPage = () => {
+const ColorPage =async () => {
+    const data = await prisma.color.findMany();
+    console.log(data);
+    
   return (
     <div>
       <ColorComponent />
