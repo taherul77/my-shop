3ccbe40be102bba38638d6768c17fdd146b9e270
SK Taherul Islam Taz <taherul15-2317@diu.edu.bd>
2025-01-23 13:44:07 +0600

    update project

--- src/app/(admin)/admin/category/page.tsx
+++ src/app/(admin)/admin/category/page.tsx
@@ -8,11 +8,10 @@ const CategoryPage = async () => {
   return (
     <>
       <div className="flex min-h-screen flex-col items-center px-6">
-      <div className=" max-w-7xl flex flex-col gap-5 w-full py-4">
-      <CategoryComponent data={data} />
+        <div className=" max-w-7xl flex flex-col gap-5 w-full py-4">
+          <CategoryComponent data={data} />
+        </div>
       </div>
-    </div>
-     
     </>
   );
 };
--- src/app/(admin)/admin/product/page.tsx
+++ src/app/(admin)/admin/product/page.tsx
@@ -5,11 +5,14 @@ import { prisma } from "../../../../../prisma/client";
 const ProductPage = async () => {
   const data = await prisma.product.findMany();
 
-
   return (
-    <div>
-      <ProductComponent data={data} />
-    </div>
+    <>
+      <div className="flex min-h-screen flex-col items-center px-6">
+        <div className=" max-w-7xl flex flex-col gap-5 w-full py-4">
+          <ProductComponent data={data} />
+        </div>
+      </div>
+    </>
   );
 };
 
