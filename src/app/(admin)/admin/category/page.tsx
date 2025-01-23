85adc715b11f4cbfaf7de8d80f6baa84bf2d7d51
SK Taherul Islam Taz <taherul15-2317@diu.edu.bd>
2025-01-23 13:42:33 +0600

    update project

--- src/app/(admin)/admin/category/page.tsx
+++ src/app/(admin)/admin/category/page.tsx
@@ -6,9 +6,14 @@ const CategoryPage = async () => {
   const data = await prisma.category.findMany();
 
   return (
-    <div>
+    <>
+      <div className="flex min-h-screen flex-col items-center px-6">
+      <div className=" max-w-7xl flex flex-col gap-5 w-full py-4">
       <CategoryComponent data={data} />
+      </div>
     </div>
+     
+    </>
   );
 };
 
