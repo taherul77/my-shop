e20ed153801362b990a234f68a73ab86442d0654
SK Taherul Islam Taz <taherul15-2317@diu.edu.bd>
2025-01-30 12:16:02 +0600

    add product page

--- src/components/main/MainNav/MainNav.tsx
+++ src/components/main/MainNav/MainNav.tsx
@@ -41,34 +41,9 @@ function Navbar({ className }: { className?: string }) {
               <HoveredLink href="/branding">Branding</HoveredLink>
             </div>
           </MenuItem>
-          <MenuItem setActive={setActive} active={active} item="Products">
-            <div className="text-sm grid grid-cols-2 gap-10 p-4">
-              <ProductItem
-                title="Algochurn"
-                href="https://algochurn.com"
-                src="https://assets.aceternity.com/demos/algochurn.webp"
-                description="Prepare for tech interviews like never before."
-              />
-              <ProductItem
-                title="Tailwind Master Kit"
-                href="https://tailwindmasterkit.com"
-                src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
-                description="Production ready Tailwind css components for your next project"
-              />
-              <ProductItem
-                title="Moonbeam"
-                href="https://gomoonbeam.com"
-                src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
-                description="Never write from scratch again. Go from idea to blog in minutes."
-              />
-              <ProductItem
-                title="Rogue"
-                href="https://userogue.com"
-                src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png"
-                description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
-              />
-            </div>
-          </MenuItem>
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/products">Products</HoveredLink>
+          </div>
           <MenuItem setActive={setActive} active={active} item="Pricing">
             <div className="flex flex-col space-y-4 text-sm">
               <HoveredLink href="/hobby">Hobby</HoveredLink>
@@ -77,14 +52,12 @@ function Navbar({ className }: { className?: string }) {
               <HoveredLink href="/enterprise">Enterprise</HoveredLink>
             </div>
           </MenuItem>
-          <MenuItem setActive={setActive} active={active} item="Contact">
-            <div className="flex flex-col space-y-4 text-sm">
-              <HoveredLink href="/contact">Contact Us</HoveredLink>
-            </div>
-          </MenuItem>
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/contact">Contact Us</HoveredLink>
+          </div>
         </div>
         <div className="flex justify-between items-center gap-4">
-            by
+          by
         </div>
       </Menu>
     </div>
