04dcf18e8150b265554ae3c64671790bc85d562d
SK Taherul Islam Taz <taherul15-2317@diu.edu.bd>
2025-02-03 15:20:14 +0600

    add brand

--- src/components/main/Home/Category/Category.tsx
+++ src/components/main/Home/Category/Category.tsx
@@ -1,3 +1,4 @@
+import { prisma } from "../../../../../prisma/client";
 import ProductCard from "./ProductCard";
 
 export interface ProductDetails {
@@ -6,54 +7,24 @@ export interface ProductDetails {
   link: string;
 }
 
-const products = [
-  {
-    name: "KITCHEN AREA",
-    imageUrl: "/category/category1.jpg",
-    link: "/expertise/kitchen-area",
-  },
-  {
-    name: "COUNTER TOP",
-    imageUrl: "/category/catecory2.jpg",
-    link: "/expertise/counter-top",
-  },
-  {
-    name: "WARDROBES",
-    imageUrl: "/category/category3.jpg",
-    link: "/expertise/wardrobes",
-  },
-  {
-    name: "WALK-IN CLOSETS",
-    imageUrl: "/category/category4.jpeg",
-    link: "/expertise/walk-in-closets",
-  },
-  {
-    name: "VANITIES",
-    imageUrl: "/category/category5.jpeg",
-    link: "/expertise/vanities",
-  },
-  {
-    name: "ACCESSORIES",
-    imageUrl: "/category/category6.jpeg",
-    link: "/expertise/accessories",
-  },
-];
+const Category = async () => {
+  const brandData = await prisma.brand.findMany();
+  console.log(brandData);
 
-const Category = () => {
   return (
     <div className="container max-auto max-w-6xl">
       <div className="flex justify-center lg:justify-start items-center py-10">
         <div className="text-xl md:text-3xl lg:text-5xl font-normal text-black dark:text-white">
-          Our <span className="text-brandColor">C</span>ategory
+          Our <span className="text-brandColor">B</span>rand
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {products.map((product: ProductDetails, index: number) => (
+        {brandData.map((brand) => (
           <ProductCard
-            key={index}
-            name={product.name}
-            link={product.link}
-            imageUrl={product.imageUrl}
+            key={brand.id}
+            name={brand.name}
+            link={`/brands/${brand.id}`}
+            imageUrl={brand.image ?? 'https://res.cloudinary.com/dkvqtc5pb/image/upload/v1737433308/products/1737433306032-taherul.PNG.png'}
           />
         ))}
       </div>
